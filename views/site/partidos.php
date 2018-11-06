e30ed1f7e4a5e2fa83adc4793360d9a215866470
Emilio <emiliops11@gmail.com>
2018-11-05 21:26:03 -0300

    modificado listado partidos

--- views/site/partidos.php
+++ views/site/partidos.php
@@ -35,11 +35,24 @@ $this->params['breadcrumbs'][] = $this->title;
                                     <p><?php echo FAS::i('calendar-alt');?> Fecha: 13/03/2018</p>
                                     <p><?php echo FAS::i('clock');?> Hora: 13:00 </p>
                                     <p><?php echo FAS::i('users');?> Jueces: Andreas Egli, Anne Ullrich, Jake Garner, John Blom, Greg Allensworth</p>
-                                    <p><a href="#" class="btn btn-primary" role="button">Ver más</a></p>
+                                    <p class="text-center">
+                                    <?php 
+                                        echo Html::a('Ver detalle',
+                                            [
+                                            'partido',
+                                            'j1'=> 'Juan Martín del Potro',
+                                            'j2'=> 'Stanislas Wawrinka',
+                                            
+                                            ],
+                                            [
+                                            'class'=>'btn btn-lg btn-primary btnPartido',
+                                            ]);
+                                    ?> 
+                                    </p> 
                                 </div>
                                 <div class="col-lg-6">
-                                    <div class="col-lg-6"><img src="img/delPotro.png" alt="..." class="img-rounded imagenJugador"></div>
-                                    <div class="col-lg-6"><img src="img/wawrinka.png" alt="..." class="img-rounded imagenJugador"></div>
+                                    <div class="col-lg-6"><img src="img/Juan Martín del Potro.png" alt="..." class="img-rounded imagenJugador"></div>
+                                    <div class="col-lg-6"><img src="img/Stanislas Wawrinka.png" alt="..." class="img-rounded imagenJugador"></div>
                                 </div>
                             </div>
                         </div>
@@ -54,8 +67,18 @@ $this->params['breadcrumbs'][] = $this->title;
                                     <p><span class="glyphicon glyphicon-stats" aria-hidden="true"></span> Resultado pacial: 4–6 </p>
                                     <p><?php echo FAS::i('table-tennis');?> Disputado en la cancha N°2</p>
                                     <p><?php echo FAS::i('users');?> Jueces: Andreas Egli, Anne Ullrich, Jake Garner, John Blom, Greg Allensworth</p>
-                                    <p><a href="#" class="btn btn-primary" role="button">Ver más</a></p>
-                                </div>
+                                    <p class="text-center">
+                                    <?php 
+                                        echo Html::a('Ver detalle',
+                                            [
+                                            'partido',
+                                            'idCancha'=> 3,
+                                            ],
+                                            [
+                                            'class'=>'btn btn-lg btn-primary btnPartido',
+                                            ]);
+                                    ?> 
+                                    </p>                                 </div>
                                 <div class="col-lg-6">
                                         <div class="col-lg-6"><img src="img/federer.png" alt="..." class="img-rounded imagenJugador"></div>
                                         <div class="col-lg-6"><img src="img/nadal.png" alt="..." class="img-rounded imagenJugador"></div>
@@ -73,8 +96,18 @@ $this->params['breadcrumbs'][] = $this->title;
                                     <p><span class="glyphicon glyphicon-stats" aria-hidden="true"></span> Resultado pacial: 5–7, 6–4 </p>
                                     <p><?php echo FAS::i('table-tennis');?> Disputado en la cancha N°1</p>
                                     <p><?php echo FAS::i('users');?> Jueces: Andreas Egli, Anne Ullrich, Jake Garner, John Blom, Greg Allensworth</p>
-                                    <p><a href="#" class="btn btn-primary" role="button">Ver más</a></p>
-                                </div>
+                                    <p class="text-center">
+                                    <?php 
+                                        echo Html::a('Ver detalle',
+                                            [
+                                            'partido',
+                                            'idCancha'=> 3,
+                                            ],
+                                            [
+                                            'class'=>'btn btn-lg btn-primary btnPartido',
+                                            ]);
+                                    ?> 
+                                    </p>                                 </div>
                                 <div class="col-lg-6">
                                     <div class="col-lg-6"><img src="img/pell.png" alt="..." class="img-rounded imagenJugador"></div>
                                     <div class="col-lg-6"><img src="img/cilic.png" alt="..." class="img-rounded imagenJugador"></div>
@@ -92,8 +125,18 @@ $this->params['breadcrumbs'][] = $this->title;
                                     <p><span class="glyphicon glyphicon-stats" aria-hidden="true"></span> Resultado pacial: 2–6 </p>
                                     <p><?php echo FAS::i('table-tennis');?> Disputado en la cancha N°7</p>
                                     <p><?php echo FAS::i('users');?> Jueces: Andreas Egli, Anne Ullrich, Jake Garner, John Blom, Greg Allensworth</p>
-                                    <p><a href="#" class="btn btn-primary" role="button">Ver más</a></p>
-                                </div>
+                                    <p class="text-center">
+                                    <?php 
+                                        echo Html::a('Ver detalle',
+                                            [
+                                            'partido',
+                                            'idCancha'=> 3,
+                                            ],
+                                            [
+                                            'class'=>'btn btn-lg btn-primary btnPartido',
+                                            ]);
+                                    ?> 
+                                    </p>                                 </div>
                                 <div class="col-lg-6">
                                     <div class="col-lg-6"><img src="img/thiem.png" alt="..." class="img-rounded imagenJugador"></div>
                                     <div class="col-lg-6"><img src="img/djokovic.png" alt="..." class="img-rounded imagenJugador"></div>
