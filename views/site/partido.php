f437b9b72b9981dc4ec25a3bf2d17c92858aa060
Emilio Melo <emiliops11@gmail.com>
2018-11-07 21:22:08 -0300

    galeria multimedia agregada al partido

--- views/site/partido.php
+++ views/site/partido.php
@@ -150,7 +150,42 @@ $this->params['breadcrumbs'][] = $this->title;
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-6 col-lg-offset-3 text-center">
+            <div class="col-lg-6">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="/img/multimedia1.jpg" alt="...">
+                        </div>
+                        <div class="item">
+                            <img src="/img/multimedia2.jpg" alt="...">
+                        </div>
+                        <div class="item">
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/85u0eDQ_T4k"></iframe>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>            
+            </div>
+            <div class="col-lg-6 text-center">
                 <div class="well">
                     <p><h4><u>Información del partido</u></h4></p><br>
                     <p><?php echo FAS::i('calendar-alt'). ' <b>Fecha:</b> '.$fecha ?></p>
@@ -160,7 +195,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         <?php 
                             echo FAS::i('table-tennis'); 
                             echo ' <b>Disputado en la cancha N°1</b> (';
-                            echo Html::a('Ver información',
+                            echo Html::a('Ver información de la cancha',
                                 [
                                   'cancha',
                                   'idCancha'=> 1,
